e56a5b9b8456b6cf9a3a32b5684dca311466717b
Florent Flament <fflament@pivotal.io>
2018-10-03 12:10:41 +0200

    pcfbox-gcloud now allows specifying a local startup script

--- pcfbox-gcloud.sh
+++ pcfbox-gcloud.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env sh
+set -e -x
 
 ZONE=europe-west1-b
 DISK_SIZE=200GB
-STARTUP_SCRIPT_URL=https://raw.githubusercontent.com/FlorentFlament/pcf-tools/master/pcfbox-setup.bash
+DEFAULT_STARTUP=https://raw.githubusercontent.com/FlorentFlament/pcf-tools/master/pcfbox-setup.bash
+OPTS=""
+
+# If an argument is provided, use it as a startup file
+if [ $# -gt 0 ]; then
+    STARTUP_FILE=$1
+    OPTS="$OPTS --metadata-from-file=startup-script=$STARTUP_FILE"
+else
+    OPTS="$OPTS --metadata=startup-script-url=$DEFAULT_STARTUP"
+fi
 
 gcloud compute instances create pcfbox \
        --zone=$ZONE \
        --image-project=ubuntu-os-cloud \
        --image-family=ubuntu-1604-lts \
        --boot-disk-size=$DISK_SIZE \
-       --metadata=startup-script-url=$STARTUP_SCRIPT_URL
+       $OPTS
