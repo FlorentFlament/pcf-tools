b05898d5deab2a7fa8c7c650cfc9a52b6fae78c1
Florent Flament <fflament@pivotal.io>
2018-10-03 12:21:23 +0200

    pcfbox-setup.bash to install a recent docker-compose binary

--- pcfbox-setup.bash
+++ pcfbox-setup.bash
@@ -4,7 +4,7 @@
 # platform.
 
 # Basic tools
-PACKAGES="docker-compose git mysql-client traceroute"
+PACKAGES="docker.io git mysql-client traceroute"
 
 # packages needed to install cf-uaac
 PACKAGES="$PACKAGES ruby ruby-dev make g++"
@@ -12,14 +12,17 @@ PACKAGES="$PACKAGES ruby ruby-dev make g++"
 BINARIES[0]=bosh
 BINARIES[1]=pivnet
 BINARIES[2]=om
+BINARIES[3]=docker-compose
 
 BIN_URLS[0]="https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-5.1.2-linux-amd64"
 BIN_URLS[1]="https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.54/pivnet-linux-amd64-0.0.54"
 BIN_URLS[2]="https://github.com/pivotal-cf/om/releases/download/0.41.0/om-linux" 
+BIN_URLS[3]="https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64"
 
 BIN_SHAS[0]="8afb3f5b64ba365aa38acb733a57bedd789b1e27"
 BIN_SHAS[1]="fd4cbb924b50f4e3fb9487babd5c080f1d629569"
 BIN_SHAS[2]="d0d8890b014514c9fa53b70badf574807b149efa"
+BIN_SHAS[3]="9303600664184658b7124d0a65f9e12a4d672708"
 
 function download_file() {
 	BIN=$1
@@ -43,7 +46,7 @@ echo "Installing distribution packages: $PACKAGES"
 apt install -y $PACKAGES
 
 echo "Installing binaries from internet"
-for i in {0..2}; do
+for i in {0..3}; do
 	BIN=${BINARIES[$i]}
 	if ! which $BIN; then
 		download_file $BIN ${BIN_SHAS[$i]} ${BIN_URLS[$i]}
